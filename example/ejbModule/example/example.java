de23da3ba248784cd0dab3d1c48dd60019761031
Administrator <Administrator@MD502-21>
2019-05-15 08:41:05 +0800

    hhh

---
+++ example/ejbModule/example/example.java
@@ -0,0 +1,17 @@
+package example;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class example
+ */
+@Stateless
+public class example implements exampleLocal {
+
+	/**
+     * Default constructor. 
+     */
+    public example() {
+        
+    }
+}
---
+++ example/ejbModule/example/exampleLocal.java
@@ -0,0 +1,11 @@
+package example;
+
+import javax.ejb.Local;
+
+/**
+ * example local interface
+ */
+@Local
+public interface exampleLocal {
+
+}
